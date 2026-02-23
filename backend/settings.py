bd4b55b19eaf8814c2cb2dca07af715fac0f8156
vpmx <vpmx555@gmail.com>
2026-02-23 15:02:22 +0700

    switch to posgre

--- backend/settings.py
+++ backend/settings.py
@@ -92,11 +92,15 @@ SESSION_COOKIE_SECURE = True
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
