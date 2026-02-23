ddad267a9db704365313f1e65cd719a55ca72c01
vpmx <vpmx555@gmail.com>
2026-02-23 15:23:24 +0700

    change data

--- backend/settings.py
+++ backend/settings.py
@@ -94,14 +94,25 @@ SESSION_COOKIE_SECURE = True
 
 import dj_database_url
 import os
+from pathlib import Path
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True,
-    )
-}
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.environ.get("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
